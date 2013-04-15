0de9c3e225c27b665a7eca6382178d533479f3e2
gggeek <giunta.gaetano@gmail.com>
2013-04-15 17:03:40 +0200

    more renaming

--- ezinfo.php
+++ ezinfo.php
@@ -3,7 +3,7 @@ class ggsysinfoInfo
 {
     static function info()
     {
-        return array( 'Name' => "<a href=\"http://projects.ez.no/ggsysinfo\">eZ SystemReport</a>",
+        return array( 'Name' => "eZ SystemReport",
                       'Version' => "0.7-dev",
                       'Copyright' => "Copyright (C) 2008-2013 eZ Systems AS",
                       'License' => "GNU General Public License v2.0"
